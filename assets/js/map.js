66d488dcb1d97ed6acddcd21bcec7d9f1074dc1f
Nathan <nmalcolm@users.noreply.github.com>
2025-12-28 15:45:16 +0400

    Create map.js

---
+++ assets/js/map.js
@@ -0,0 +1,46 @@
+$(function() {
+  var visited = "#2ecc71";
+  var home = "#3498db";
+
+  var data = {
+    "GB": home,
+    "UA": visited,
+    "TH": visited,
+    "MY": visited,
+    "BN": visited,
+    "ID": visited,
+    "KR": visited,
+    "TW": visited,
+    "PH": visited,
+    "GE": visited,
+    "PK": visited,
+    "LA": visited,
+    "VN": visited,
+    "KH": visited,
+    "UZ": visited,
+    "KZ": visited,
+    "AM": visited,
+    "TR": visited
+  };
+
+  $('#count').text(Object.keys(data).length);
+
+  $('#vmap').vectorMap({
+    map: 'world_mill',
+    backgroundColor: '#0f2744',
+    series: {
+      regions: [{
+        values: data,
+        attribute: 'fill'
+      }]
+    }, onRegionTipShow: function(e, label, code) {
+      if (code == "GB") {
+        label.html('<div class="map-tooltip">' + label.html() + ' is my home country.</div>');
+      } else if (code in data) {
+        label.html('<div class="map-tooltip">I\'ve visited ' + label.html() + '!</div>');
+      } else {
+        label.html('<div class="map-tooltip">' + label.html() + ': I haven\'t visited this country yet!</div>');
+      }
+    }
+  });
+});
